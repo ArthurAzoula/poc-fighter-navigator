cd34fca7634a8de69d11489049b8dd04a539fa63
Arthur Azoula <a.azoula@faceel-it.com>
2024-06-10 16:04:58 +0200

    fix: add url

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://poc-fighter-navigator-front.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
--- front/src/socket.js
+++
@@ -1,5 +0,0 @@
-import { io } from 'socket.io-client';
-
-const URL = 'https://poc-fighter-navigator-back.vercel.app/';
-
-export const socket = io(URL);
