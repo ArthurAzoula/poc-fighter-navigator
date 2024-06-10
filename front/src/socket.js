e6846b58418285cd11558a14c57b88bf63049ed4
Arthur Azoula <a.azoula@faceel-it.com>
2024-06-10 15:47:35 +0200

    feature: add url

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:4000';
+const URL = 'https://poc-fighter-navigator-jekm-back-iwota6nda.vercel.app/';
 
 export const socket = io(URL);
